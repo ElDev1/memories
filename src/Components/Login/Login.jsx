5095964b63e36551f3f6bc3040439788b5de7ad9
estebanzarate <esteban-zarate@hotmail.com>
2022-09-10 23:06:50 -0300

    add yup validation, finish form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,27 +1,72 @@
+import './Login.css';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
+    const initialValues = {
+        email: '',
+        password: ''
+    };
+
+    const onSubmit = values => console.log(values);
+
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .email('Invalid email address')
+            .required('Required field'),
+        password: Yup.string().required('Required field')
+    });
+
+    const formik = useFormik({ initialValues, onSubmit, validationSchema });
+    const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
+        formik;
+
     return (
-        <div>
-            <h2>Login</h2>
-            <form>
-                <div>
+        <section className='login'>
+            <h2 className='login-title'>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className='input-container'>
                     <label htmlFor='email'>Email</label>
-                    <input type='text' placeholder='Email' required />
+                    <input
+                        id='email'
+                        type='text'
+                        name='email'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        placeholder='Type your email'
+                        required
+                    />
+                    {errors.email && touched.email ? (
+                        <div>{errors.email}</div>
+                    ) : null}
                 </div>
-                <div>
+                <div className='input-container'>
                     <label htmlFor='password'>Email</label>
-                    <input type='password' placeholder='Password' required />
+                    <input
+                        id='password'
+                        type='password'
+                        name='password'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        placeholder='Type your password'
+                        required
+                    />
+                    {errors.password && touched.password ? (
+                        <div>{errors.password}</div>
+                    ) : null}
                 </div>
-                <div>
-                    <button>Login</button>
+                <div className='submit-container'>
+                    <button type='submit'>Login</button>
                 </div>
                 <div>
                     <p>Don't have an account yet</p>
                     <Link to='/register'>Register</Link>
                 </div>
             </form>
-        </div>
+        </section>
     );
 };
 
