d2329357fbe5ad7b357ba48d293dc2f99c254e49
estebanzarate <esteban-zarate@hotmail.com>
2022-09-12 15:33:36 -0300

    add img logo

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -23,49 +23,58 @@ const Login = () => {
         formik;
 
     return (
-        <section className='login'>
-            <h2 className='login-title'>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className='input-container'>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        id='email'
-                        type='text'
-                        name='email'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        placeholder='Type your email'
-                        required
-                    />
-                    {errors.email && touched.email ? (
-                        <div>{errors.email}</div>
-                    ) : null}
-                </div>
-                <div className='input-container'>
-                    <label htmlFor='password'>Email</label>
-                    <input
-                        id='password'
-                        type='password'
-                        name='password'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                        placeholder='Type your password'
-                        required
-                    />
-                    {errors.password && touched.password ? (
-                        <div>{errors.password}</div>
-                    ) : null}
-                </div>
-                <div className='submit-container'>
-                    <button type='submit'>Login</button>
-                </div>
-                <div>
-                    <p>Don't have an account yet</p>
-                    <Link to='/register'>Register</Link>
-                </div>
-            </form>
+        <section className='login-section'>
+            <div className='login-img-container'>
+                <img src={require('../../img/logo.png')} alt='Logo' />
+            </div>
+            <div className='login-container'>
+                <h2 className='login-title'>Welcome!</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className='input-container'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            id='email'
+                            type='text'
+                            name='email'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            placeholder='Type your email'
+                            required
+                        />
+                        {errors.email && touched.email ? (
+                            <div className='input-error'>{errors.email}</div>
+                        ) : null}
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            id='password'
+                            type='password'
+                            name='password'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            placeholder='Type your password'
+                            required
+                        />
+                        {errors.password && touched.password ? (
+                            <div className='input-error'>{errors.password}</div>
+                        ) : null}
+                    </div>
+                    <div className='submit-container'>
+                        <button className='submit-btn' type='submit'>
+                            Login
+                        </button>
+                    </div>
+                    <div className='register-link'>
+                        <p>Don't have an account yet</p>
+                        <Link className='link' to='/register'>
+                            register
+                        </Link>
+                    </div>
+                </form>
+            </div>
         </section>
     );
 };
