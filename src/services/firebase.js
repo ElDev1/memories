f3bf6c2e65558d7b09d7d5d2f5a05a4409374ee4
estebanzarate <esteban-zarate@hotmail.com>
2022-09-19 19:20:22 -0300

    add register new users firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,44 +1,43 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc  } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    doc
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-
-const {DATABASE_API_KEY} = process.env 
+const { REACT_APP_API_KEY } = process.env;
 
 const firebaseConfig = {
-  apiKey: {DATABASE_API_KEY},
-  authDomain: "memories-944af.firebaseapp.com",
-  projectId: "memories-944af",
-  storageBucket: "memories-944af.appspot.com",
-  messagingSenderId: "807187385306",
-  appId: "1:807187385306:web:e98ec1169c405e4ee1cee3"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: 'memories-944af.firebaseapp.com',
+    projectId: 'memories-944af',
+    storageBucket: 'memories-944af.appspot.com',
+    messagingSenderId: '807187385306',
+    appId: '1:807187385306:web:e98ec1169c405e4ee1cee3'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-
-const db = getFirestore()
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+export const auth = getAuth(app);
 
 export const createMemorie = (title, text, tags, image) => {
-  addDoc(collection(db, 'memoriesList'), {title, text, tags, image})
-}
+    addDoc(collection(db, 'memoriesList'), { title, text, tags, image });
+};
 
 export const getMemories = () => {
-  return getDocs(collection(db, 'memoriesList'))
-    .then(res => res.docs)
-    .then(res => {
-      const data = []
-      res.forEach(doc => {
-        data.push(doc.data())
-      })
-      return data
-    })
-}
+    return getDocs(collection(db, 'memoriesList'))
+        .then(res => res.docs)
+        .then(res => {
+            const data = [];
+            res.forEach(doc => {
+                data.push(doc.data());
+            });
+            return data;
+        });
+};
 
-
-export const deleteMemorie = (id) => deleteDoc(doc(db, 'memoriesList', id))
- 
-
+export const deleteMemorie = id => deleteDoc(doc(db, 'memoriesList', id));
