32d0a8bf4add7e8ea2c6978edcbd77c46d782651
Devi Amaolo <deviamaolo@gmail.com>
2022-09-19 17:33:30 -0300

    pages and routes created

---
+++ src/pages/CreateMemories/CreateMemories.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateMemories = () => {
+  return (
+    <div>CreateMemories</div>
+  )
+}
---
+++ src/pages/WorlMemories/WorldMemories.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const WorldMemories = () => {
+  return (
+    <div>WorldMemories</div>
+  )
+}
+
