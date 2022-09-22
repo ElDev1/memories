831716e164641d35e207dea4b14b0e7e62a5cd5d
Josep Palomino <jfpalomino.22@gmail.com>
2022-09-22 02:12:11 -0500

    MyMemories v1.0

---
+++ src/Components/NavBar.jsx/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div class="container-nav">
+      <nav>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/9e15/dd50/ce015638380457fdf74a147d1e823c68?Expires=1664755200&Signature=NNp6YbyjPHicFtHGFIBBI3VeYsozMgK9CqxvHcr0BqNpOaSmzhQxF4yRY1kYTc9bbfwP1fL0ed4hLh9hTphRMluvm5sokqtW5kv8KwmFeFOcukTS7E5wi1~5TuDJEbVMZovYkfsDBA2VaZgcmIXraZEqqLCYRxS8A6ycLwGt9OkNPODXVucn~mjuuKu-szJJBbdTz7J02jG-lRoDaf9sdtPhYeLlJxfP-VAgpRfGroqNIaRQSbHyRXH8imuO0zQnJQ77bdPg5vCPmEfCrHbtI0QMkQgxrknwjEcJWlduMrjqxnBPIRCn9RdpSRMjLCLo5pO7cp~zDi16nTlHR9yY0g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+          alt=""
+          className="logo-image"
+        />
+        <div className="nav-paths">
+          <a href="">World Memories</a>
+          <a href="">My Memories</a>
+          <a href="">Create Memory</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
